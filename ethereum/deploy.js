639cf17a9052cb63b1c31ec0b992284aa86a2f27
Frank Wang <frank7300228@gmail.com>
2021-07-11 20:51:06 +0800

    Finish 155

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -1,6 +1,6 @@
 const HDWalletProvider = require("truffle-hdwallet-provider")
 const Web3 = require("web3")
-const { interface, bytecode } = require("./compile")
+const compiledFactory = require("./build/CampaignFactory.json")
 const { key, url } = require("./key")
 
 const provider = new HDWalletProvider(key, url)
@@ -11,11 +11,12 @@ const deploy = async () => {
 
   console.log("Attempting to deploy from account", accounts[0])
 
-  const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode })
+  const result = await new web3.eth.Contract(
+    JSON.parse(compiledFactory.interface)
+  )
+    .deploy({ data: compiledFactory.bytecode })
     .send({ gas: "1000000", from: accounts[0] })
 
-  console.log(interface)
   console.log("Contract deployed to", result.options.address)
 }
 
