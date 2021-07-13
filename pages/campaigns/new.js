e27dd59e643fdf477eeaa626cf7a9d29d5878753
Frank Wang <frank7300228@gmail.com>
2021-07-14 01:56:45 +0800

    Finish 189

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -7,11 +7,14 @@ import web3 from "../../ethereum/web3"
 class CampaignNew extends Component {
   state = {
     minimumContribution: "",
-    errorMessage: ""
+    errorMessage: "",
+    loading: false
   }
 
   onSubmit = async e => {
     e.preventDefault()
+
+    this.setState({ loading: true, errorMessage: "" })
 
     try {
       const accounts = await web3.eth.getAccounts()
@@ -23,6 +26,8 @@ class CampaignNew extends Component {
     } catch (err) {
       this.setState({ errorMessage: err.message })
     }
+
+    this.setState({ loading: false })
   }
 
   render() {
@@ -42,7 +47,9 @@ class CampaignNew extends Component {
             />
           </Form.Field>
           <Message error header="Oops!" content={this.state.errorMessage} />
-          <Button primary>Create!</Button>
+          <Button primary loading={this.state.loading}>
+            Create!
+          </Button>
         </Form>
       </Layout>
     )
