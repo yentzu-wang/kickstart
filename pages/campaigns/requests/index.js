afd19558312ba27b0c43b1ddf7126457af5c79f2
Frank Wang <frank7300228@gmail.com>
2021-07-17 21:07:38 +0800

    Finish 220

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -2,12 +2,23 @@ import React, { Component } from "react"
 import { Button } from "semantic-ui-react"
 import Layout from "../../../components/Layout"
 import { Link } from "../../../routes"
+import Campaign from "../../../ethereum/campaign"
 
 class RequestIndex extends Component {
   static async getInitialProps(props) {
     const { address } = props.query
+    const campaign = new Campaign(address)
+    const requestCount = await campaign.methods.getRequestCount().call()
 
-    return { address }
+    const requests = await Promise.all(
+      Array(requestCount)
+        .fill()
+        .map((element, index) => {
+          return campaign.methods.requests(index).call()
+        })
+    )
+
+    return { address, requests, requestCount }
   }
 
   render() {
