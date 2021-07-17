b38beef28aa691124534ca2416eb7d52880740a5
Frank Wang <frank7300228@gmail.com>
2021-07-17 17:34:20 +0800

    Finish 203

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react"
+import { Card } from "semantic-ui-react"
 import Layout from "../../components/Layout"
 import Campaign from "../../ethereum/campaign"
 
@@ -10,16 +11,39 @@ class CampaignShow extends Component {
     return {
       minimumContribution: summary[0],
       balance: summary[1],
-      getRequestCount: summary[2],
+      requestsCount: summary[2],
       approversCount: summary[3],
       manager: summary[4]
     }
+  }
+
+  renderCards = () => {
+    const {
+      balance,
+      manager,
+      minimumContribution,
+      requestsCount,
+      approversCount
+    } = this.props
+
+    const items = [
+      {
+        header: manager,
+        meta: "Address of Manager",
+        description:
+          "The manager created this campaign and can create requests to withdraw money",
+        style: { overflowWrap: "break-word" }
+      }
+    ]
+
+    return <Card.Group items={items} />
   }
 
   render() {
     return (
       <Layout>
         <h3>Campaign Show</h3>
+        {this.renderCards()}
       </Layout>
     )
   }
