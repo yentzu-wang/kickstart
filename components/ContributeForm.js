86b84f81c915525109015d011409a83c18356401
Frank Wang <frank7300228@gmail.com>
2021-07-17 18:59:11 +0800

    Finish 211

--- components/ContributeForm.js
+++ components/ContributeForm.js
@@ -5,12 +5,14 @@ import web3 from "../ethereum/web3"
 import { Router } from "../routes"
 
 class ContributeForm extends Component {
-  state = { value: "" }
+  state = { value: "", errorMessage: "", loading: false }
 
   onSubmit = async e => {
     e.preventDefault()
 
     const campaign = Campaign(this.props.address)
+
+    this.setState({ loading: true, errorMessage: "" })
 
     try {
       const accounts = await web3.eth.getAccounts()
@@ -20,12 +22,16 @@ class ContributeForm extends Component {
       })
 
       Router.replaceRoute(`/campaigns/${this.props.address}`)
-    } catch (err) {}
+    } catch (err) {
+      this.setState({ errorMessage: err.message })
+    }
+
+    this.setState({ loading: false, value: "" })
   }
 
   render() {
     return (
-      <Form onSubmit={this.onSubmit}>
+      <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
         <Form.Field>
           <label>Amount to Contribute</label>
           <Input
@@ -35,7 +41,10 @@ class ContributeForm extends Component {
             labelPosition="right"
           />
         </Form.Field>
-        <Button primary>Contribute</Button>
+        <Message error header="Oops" content={this.state.errorMessage} />
+        <Button primary loading={this.state.loading}>
+          Contribute
+        </Button>
       </Form>
     )
   }
